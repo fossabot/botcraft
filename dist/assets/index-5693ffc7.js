eb9c52440c55db286fad4d71ce4312890b042acf
Eva1ent <let@ik.me>
2023-06-02 22:59:13 +0800

    Update build

---
+++ dist/assets/index-5693ffc7.js
@@ -0,0 +1,1 @@
+import{p as o,t as r,q as e}from"./index-3f086703.js";import{a as t}from"./react-1b326164.js";const a=o(t.memo,s=>e.jsx("div",{children:e.jsx("h1",{children:"404"})})),n=()=>{let s=r(t.useRef,1);return 0 in s?s[0]:s[0]=e.jsx(a,{v:[]})};export{n as default};
