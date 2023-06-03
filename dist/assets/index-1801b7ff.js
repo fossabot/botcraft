ba69364be1bf84052d5a93b0bc813dc9db0ae1f9
Eva1ent <let@ik.me>
2023-06-03 09:03:14 +0800

    Update build

---
+++ dist/assets/index-1801b7ff.js
@@ -0,0 +1,1 @@
+import{p as o,t as r,q as e}from"./index-0ed8eca6.js";import{a as t}from"./react-5e019c7a.js";const a=o(t.memo,s=>e.jsx("div",{children:e.jsx("h1",{children:"404"})})),n=()=>{let s=r(t.useRef,1);return 0 in s?s[0]:s[0]=e.jsx(a,{v:[]})};export{n as default};
--- dist/assets/index-2703f051.js
+++
@@ -1,1 +0,0 @@
-import{p as o,t as r,q as e}from"./index-8ea6f686.js";import{a as t}from"./react-5e019c7a.js";const a=o(t.memo,s=>e.jsx("div",{children:e.jsx("h1",{children:"404"})})),n=()=>{let s=r(t.useRef,1);return 0 in s?s[0]:s[0]=e.jsx(a,{v:[]})};export{n as default};
